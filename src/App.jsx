c2a5646074928cc846dd32fa9fa5966487c29746
krrakshit <rkrkr365@gmail.com>
2025-04-27 11:10:04 +0530

    Implement user authentication in App component with Firebase, displaying login status and logout functionality.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
+import { useEffect,useState } from 'react'
 import './App.css'
 import { Signup } from './Signup'
 import { Signin } from './Signin'
+import { getAuth, onAuthStateChanged,signOut } from 'firebase/auth'
+import { firebase } from './firebase'
+
+const auth = getAuth(firebase)
 
 function App() {
-  return (
-    <>
-      <Signup />
-      <Signin/>
-    </>
-  )
+
+  const [user,setUser] = useState(null);
+
+  useEffect(()=>{
+    onAuthStateChanged(auth,(user) =>{
+      if(user){
+        setUser(user)
+      }
+      else{
+        setUser(null)
+      }
+    })
+
+  },[])
+
+
+
+  if(user === null){
+    return (
+      <>
+        <Signup />
+        <Signin/> 
+      </>
+    )  
+  }
+  else{
+    return(
+      <>
+      Hello user! you are logged in {user.email}
+      <button onClick={() => signOut(auth)}>Logout</button>
+      </>
+    )
+  }
 }
 
-export default App
+export default App  
